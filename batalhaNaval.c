bcb2a6228912eaea3817342f0ec342ef1763e764
Ti460.1911 <ti460.1911@gmail.com>
2025-05-30 14:59:06 -0300

    Simples BatalhaNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,98 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAM 10
+
+void inicializarTabuleiro(int tabuleiro[TAM][TAM]) {
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
+            tabuleiro[i][j] = 0;
+}
+
+void posicionarNavios(int tabuleiro[TAM][TAM]) {
+    // Navio horizontal
+    for (int j = 1; j <= 4; j++)
+        tabuleiro[2][j] = 3;
+
+    // Navio vertical
+    for (int i = 5; i <= 8; i++)
+        tabuleiro[i][7] = 3;
+
+    // Diagonal principal
+    for (int i = 0; i < 4; i++)
+        tabuleiro[i][i] = 3;
+
+    // Diagonal secundária
+    for (int i = 0; i < 4; i++)
+        tabuleiro[i][9 - i] = 3;
+}
+
+void exibirTabuleiro(int tabuleiro[TAM][TAM]) {
+    printf("Tabuleiro com navios (3):\n");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void aplicarHabilidade(int ataque[TAM][TAM], int tipo, int centroX, int centroY) {
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            switch (tipo) {
+                case 1: // Cone para baixo
+                    if (i >= centroX && abs(j - centroY) <= (i - centroX))
+                        ataque[i][j] = 1;
+                    break;
+                case 2: // Cruz
+                    if (i == centroX || j == centroY)
+                        ataque[i][j] = 1;
+                    break;
+                case 3: // Octaedro
+                    if (abs(i - centroX) + abs(j - centroY) <= 2)
+                        ataque[i][j] = 1;
+                    break;
+            }
+        }
+    }
+}
+
+void exibirResultado(int tabuleiro[TAM][TAM], int ataque[TAM][TAM]) {
+    printf("Resultado do ataque (X = acerto, * = erro):\n");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            if (ataque[i][j] == 1) {
+                if (tabuleiro[i][j] == 3)
+                    printf("X ");
+                else
+                    printf("* ");
+            } else {
+                printf(". ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int tabuleiro[TAM][TAM];
+    int ataque[TAM][TAM] = {0};
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    inicializarTabuleiro(tabuleiro);
+    posicionarNavios(tabuleiro);
+    exibirTabuleiro(tabuleiro);
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    printf("Escolha uma habilidade de ataque:\n");
+    printf("1 - Cone\n2 - Cruz\n3 - Octaedro\n> ");
+    int tipoAtaque;
+    scanf("%d", &tipoAtaque);
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    int centroX = 5, centroY = 5; // Centro padrão do ataque
+    aplicarHabilidade(ataque, tipoAtaque, centroX, centroY);
+    exibirResultado(tabuleiro, ataque);
 
     return 0;
 }
